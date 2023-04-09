f336c82517e88495fe9e1a1861175e80b5745936
vrmcardoso <vrmcardoso@gmail.com>
2023-04-09 01:21:37 +0100

    Started index layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,12 @@ import '../stylesheets/bootstrap.scss';
 
 import Rails from '@rails/ujs';
 Rails.start();
+
+import { Application } from 'stimulus';
+import { definitionsFromContext } from 'stimulus/webpack-helpers';
+
+const application = Application.start();
+const context = require.context('../controllers', true, /\.js$/);
+application.load(definitionsFromContext(context));
+
+import "controllers"
